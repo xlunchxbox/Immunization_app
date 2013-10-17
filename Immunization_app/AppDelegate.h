8d59d4aecdf0b1d323a8c122eb8ffe94d7b9d24f
Christopher Cruz <chris1cruz@gmail.com>
2013-10-17 10:22:38 -0500

    readded project

---
+++ Immunization_app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Immunization_app
+//
+//  Created by Christopher Cruz on 10/17/13.
+//  Copyright (c) 2013 Team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Immunization_app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Immunization_app
+//
+//  Created by Christopher Cruz on 10/17/13.
+//  Copyright (c) 2013 Team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
