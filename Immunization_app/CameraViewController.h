f7823eb50a97d0233cd99037f4bc3ccde2ed80ef
Christopher Cruz <chris1cruz@gmail.com>
2013-10-21 12:33:24 -0500

    added camera controller class and view in storyboard, currently holds a button to home but links to it should work

---
+++ Immunization_app/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Immunization_app
+//
+//  Created by Christopher Cruz on 10/21/13.
+//  Copyright (c) 2013 Team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
