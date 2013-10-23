7c3366f9e2b93f0fca57601cb92f64296cce6161
Christopher Cruz <chris1cruz@gmail.com>
2013-10-22 20:44:23 -0500

    partial camera implementation and layout for camera view. saves to CR, and can pull from CR

--- Immunization_app/CameraViewController.h
+++ Immunization_app/CameraViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MobileCoreServices/MobileCoreServices.h"
 
 @interface CameraViewController : UIViewController
+<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+
+
+@property BOOL newMedia;
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)useCamera:(id)sender;
+- (IBAction)useCameraRoll:(id)sender;
+
 
 @end
