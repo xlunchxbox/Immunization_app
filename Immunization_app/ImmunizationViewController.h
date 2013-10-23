99121139d1bb6d37507c252ecff0e30e084551d7
Christopher Cruz <chris1cruz@gmail.com>
2013-10-23 17:07:30 -0500

    attemping UIScroll

--- Immunization_app/ImmunizationViewController.h
+++ Immunization_app/ImmunizationViewController.h
@@ -21,4 +21,36 @@
 - (IBAction)uploadPhotoBtn:(id)sender;
 - (IBAction)logoutBtn:(id)sender;
 
+
+//Birth
+@property (weak, nonatomic) IBOutlet UITextField *birthBcgDa;
+
+//6 Weeks
+@property (weak, nonatomic) IBOutlet UITextField *sixWeeksPcvDa;
+
+
+
+//10 Weeks
+@property (weak, nonatomic) IBOutlet UITextField *tenWeeksPcvDa;
+
+
+//14 Weeks
+@property (weak, nonatomic) IBOutlet UITextField *fourteenWeeksPcvDa;
+
+
+//6 - 12 Months
+@property (weak, nonatomic) IBOutlet UITextField *sixTwelveInfluenzaDa;
+
+
+//15 - 18 Months
+@property (weak, nonatomic) IBOutlet UITextField *fifteenEighteenHibDa;
+
+
+//2 - 5 Years
+@property (weak, nonatomic) IBOutlet UITextField *twoFiveDtapDa;
+
+//9 - 13 Years
+@property (weak, nonatomic) IBOutlet UITextField *nineThirteenDtapDa;
+
+
 @end
